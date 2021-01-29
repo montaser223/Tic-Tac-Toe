85bbdb75785ff8b4f306f9b627d1d476876a5184
emansoliman <37782782+emansoliman@users.noreply.github.com>
2021-01-30 00:40:37 +0200

    Add files via upload

---
+++ Request.java
@@ -0,0 +1,23 @@
+package libs;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Eman
+ */
+public class Request {
+    
+
+
+    public static final String LOGIN = "login";
+    public static final String LOGOUT = "logout";
+    public static final String GAME = "game";
+    public static final String GAMEINVITATION = "gameInvitation";
+    public static final String GAMERESPOND = "Gameresponse";
+    public static final String SIGNUP = "signup";
+}
---
+++ Status.java
@@ -0,0 +1,19 @@
+package libs;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Eman
+ */
+public class Status {
+    
+
+    public static final String ONLINE = "online";
+    public static final String OFFLINE = "ofline";
+    public static final String PLAYING = "playing";
+}
