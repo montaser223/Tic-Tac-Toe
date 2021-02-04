8d4af9a69b57cb75bad8b94563d4c67158cb9081
HebaMostafa123 <37883682+HebaMostafa123@users.noreply.github.com>
2021-02-05 01:32:12 +0200

    Add files via upload

--- Client/src/libs/Request.java
+++ Client/src/libs/Request.java
@@ -16,6 +16,7 @@ public class Request {
     public static final String SIGNUP = "signup";
     public static final String PlAYER_LIST = "playerList";
     public static final String UPDATE_SCORE = "updateScore";
+    public static final String RECEVE_GAME_SYMBOL = "receveGameSymbol";
     public static final String GAME_MOVE = "gameMove";
     public static final String GAME_PLAYAGAIN = "playAgain";
     public static final String GAME_INVITATION = "gameInvitation";
@@ -27,5 +28,6 @@ public class Request {
     public static final String RECORD_GAME = "recordGame";
     public static final String GET_RECORDEDGAME = "getRecordedGame";
     public static final String DISCONNECT = "disconnect";
+    public static final String STOPGAME = "stopgame";
 
 }
